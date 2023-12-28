986889e8cf3bc6726c759dd67f7a8aa326e8dae5
perezmarianopm <perezmariano.pm@gmail.com>
2023-12-28 16:52:10 -0300

    changes font

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -2,7 +2,7 @@ import { createGlobalStyle } from "styled-components"
 
 export const GlobalStyles = createGlobalStyle`
     @import url('https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap');
-    
+
     :root {
         --primary: wheat;
         --secondary: #111A25;
@@ -14,7 +14,7 @@ export const GlobalStyles = createGlobalStyle`
         box-sizing: border-box;
         text-decoration: none;
         list-style-type: none;
-        font-family: 'Open Sans';
+        font-family: 'Open Sans', 'Montserrat';
     }
 
     main {
